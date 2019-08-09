e8094cd4627faa891d0953cbfb2da13c3f6de791
tejasgawali <tejas.gawali4@gmail.com>
2019-08-09 23:49:39 +0530

    Updates

---
+++ precache-manifest.b91c11cb65d96466045b741773d34166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0b7942abb300947df7bec3285b3d2c",
+    "url": "/MERN-Full-Stack-App/index.html"
+  },
+  {
+    "revision": "5ffae26f9ce0d64b0ef8",
+    "url": "/MERN-Full-Stack-App/static/css/main.d8e0a91e.chunk.css"
+  },
+  {
+    "revision": "bd8976dd1b0f035c3bd2",
+    "url": "/MERN-Full-Stack-App/static/js/2.055a4b6e.chunk.js"
+  },
+  {
+    "revision": "5ffae26f9ce0d64b0ef8",
+    "url": "/MERN-Full-Stack-App/static/js/main.00307dca.chunk.js"
+  },
+  {
+    "revision": "201eab7e983d2868c1d6",
+    "url": "/MERN-Full-Stack-App/static/js/runtime~main.55dcbac5.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/MERN-Full-Stack-App/static/media/showcase.4b31330b.jpg"
+  }
+]);
