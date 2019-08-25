8907389c34d0a1baa739907e0f9a40721aacf044
tejasgawali <tejas.gawali4@gmail.com>
2019-08-26 01:03:51 +0530

    WIP

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,85 +1,75 @@
-import React, { Fragment , useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
 
-const Login = () => {
+  const { email, password } = formData;
 
-    const [formData, setFormData] = useState({
-        name : '',
-        email: ''
-    });
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const { email , password } = formData; 
+  const onSubmit = async e => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-    const onChange = e => setFormData({ ...formData , [e.target.name]: e.target.value });
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
-    const onSubmit = async e => {
-        e.preventDefault();
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
 
-        const newUser = {
-            email,
-            password
-        }
-        
-        try {
-            const config = {
-                headers : {
-                    'Content-Type':'application/json'
-                        }
-            }
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
-            const body = JSON.stringify(newUser);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
 
-            //console.log(body);
-
-            const res = await axios.post('/api/auth', newUser , config);                
-
-            console.log(res.data);
-            //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7I…zQyfQ.AGJpQOO0Z_eB4LpOU28qwmrReLMHQgXKgaaz4XfJ4T0
-
-        } catch (err) {
-            console.log(err.response);
-        }
-    };     
-
-    return (
-        <Fragment>
-            <div>
-            <h1 className="large text-primary">Sign In</h1>
-                <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
-                <form className="form" onSubmit = { e => onSubmit(e)}>
-                    <div className="form-group">
-                    <input type="email" 
-                        placeholder="Email Address" 
-                        name="email"
-                        value={email}
-                        onChange={e => onChange(e)}
-                        required />
-                    <small className="form-text"
-                        >This site uses Gravatar so if you want a profile image, use a
-                        Gravatar email</small
-                    >
-                    </div>
-                    <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        minLength="6"
-                        value={password}
-                        onChange={e => onChange(e)}
-                        required
-                    />
-                    </div>
-                    <input type="submit" className="btn btn-primary" value="Login" />
-                </form>
-                <p className="my-1">
-                    Create an account? <Link to="register">Register</Link>
-                </p>
-            </div>
-        </Fragment>
-    )
-}
-
-export default Login;
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
